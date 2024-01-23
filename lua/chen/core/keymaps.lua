17fd027c2f0a866ffba29ac205dc43891fe452ec
kevinyyca <121273910+kevinyyca@users.noreply.github.com>
2024-01-23 20:44:53 +0800

    Configured number keymaps

--- lua/chen/core/keymaps.lua
+++ lua/chen/core/keymaps.lua
@@ -1,3 +1,7 @@
 local keymap = vim.keymap
 
 vim.g.mapleader = " "
+
+--- Numbers ---
+keymap.set("n", "<leader>=", "<C-a>", { desc = "Increment number at cursor" }) -- Increment number at cursor
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number at cursor" }) -- Decrement number at cursor
