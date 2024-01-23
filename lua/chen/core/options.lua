3714ab2cfb0d920877d672862a5d9a432422cd0b
kevinyyca <121273910+kevinyyca@users.noreply.github.com>
2024-01-23 20:40:27 +0800

    Configured line numbers

---
+++ lua/chen/core/options.lua
@@ -0,0 +1,5 @@
+local opt = vim.opt
+
+--- Line numbers ---
+opt.relativenumber = true -- Show relative line numbers
+opt.number = true -- Shows absolute line numbers on cursor line (When relative number is off all the other lines show absolute line numbers instead of relative)
