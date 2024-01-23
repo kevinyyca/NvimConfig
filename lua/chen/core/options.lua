915196979c378477e14c7b56bf80d99424be7c2d
kevinyyca <121273910+kevinyyca@users.noreply.github.com>
2024-01-23 20:40:50 +0800

    Configured tabs and indentation

--- lua/chen/core/options.lua
+++ lua/chen/core/options.lua
@@ -3,3 +3,9 @@ local opt = vim.opt
 --- Line numbers ---
 opt.relativenumber = true -- Show relative line numbers
 opt.number = true -- Shows absolute line numbers on cursor line (When relative number is off all the other lines show absolute line numbers instead of relative)
+
+--- Tabs and indentation ---
+opt.tabstop = 2 -- Two spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- Two spaces for indent width
+opt.expandtab = true -- Expand tabs to spaces
+opt.autoindent = true -- Copy indent from current line when starting new one (Automatically create tabs when creating new lines)
