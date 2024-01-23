c05c6403900714af5b5ae1726e6c7c6f764a0119
kevinyyca <121273910+kevinyyca@users.noreply.github.com>
2024-01-23 20:45:48 +0800

    Configured split window managment keymaps

--- lua/chen/core/keymaps.lua
+++ lua/chen/core/keymaps.lua
@@ -5,3 +5,9 @@ vim.g.mapleader = " "
 --- Numbers ---
 keymap.set("n", "<leader>=", "<C-a>", { desc = "Increment number at cursor" }) -- Increment number at cursor
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number at cursor" }) -- Decrement number at cursor
+
+--- Split window managment ---
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- Split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- Split window horizontally
+keymap.set("n", "<leader>s=", "<C-w>=", { desc = "Make split windows equal size" }) -- Make split windows equal size
+keymap.set("n", "<leader>sx", ":close<CR>", { desc = "Close current split window" }) -- Close current split window
