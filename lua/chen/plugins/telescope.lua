822c9f651de112233c593041f0bc90402f3624d6
kevinyyca <121273910+kevinyyca@users.noreply.github.com>
2024-01-24 20:27:50 +0800

    Configure telescope

--- lua/chen/plugins/telescope.lua
+++ lua/chen/plugins/telescope.lua
@@ -10,6 +10,19 @@ return {
     local telescope = require("telescope")
     local actions = require("telescope.actions")
 
-    telescope.setup({})
+    telescope.setup({
+          defaults = {
+            path_display = { "truncate " },
+            mappings = {
+              i = {
+                ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+                ["<C-j>"] = actions.move_selection_next, -- move to next result
+                ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+              },
+            },
+          },
+        })
+
+    telescope.load_extension("fzf")
   end,
 }
