2fc821953f024ddc3739517ef14ba2686972ab3f
kevinyyca <121273910+kevinyyca@users.noreply.github.com>
2024-01-26 17:32:52 +0800

    Installed nvimdv/dashboard-nvim

---
+++ lua/chen/plugins/dashboard.lua
@@ -0,0 +1,9 @@
+return {
+  'nvimdev/dashboard-nvim',
+  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  event = 'VimEnter',
+  config = function()
+    local dashboard = require("dashboard")
+    dashboard.setup()
+  end,
+}
