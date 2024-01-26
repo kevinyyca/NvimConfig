cc9e5ca56ab827b2344cb5b54ebb4212b1b69662
kevinyyca <121273910+kevinyyca@users.noreply.github.com>
2024-01-26 17:19:48 +0800

    Don't include windwp/nvim-ts-autotag in init.lua

--- lua/chen/plugins/init.lua
+++ lua/chen/plugins/init.lua
@@ -2,5 +2,4 @@ return {
   "christoomey/vim-tmux-navigator",  -- Tmux and spit window navigation
   "tpope/vim-surround",              -- Sourround text
   "vim-scripts/ReplaceWithRegister", -- Replace text with copied text
-  "windwp/nvim-ts-autotag"
 }
