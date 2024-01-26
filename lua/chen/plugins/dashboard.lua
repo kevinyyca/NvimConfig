6144a2891af226113c849f37c59875fb82545fed
kevinyyca <121273910+kevinyyca@users.noreply.github.com>
2024-01-26 17:38:04 +0800

    Make format of dashboard.lua nicer

--- lua/chen/plugins/dashboard.lua
+++ lua/chen/plugins/dashboard.lua
@@ -1,7 +1,8 @@
 return {
   'nvimdev/dashboard-nvim',
-  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   event = 'VimEnter',
+
   config = function()
     local dashboard = require("dashboard")
     dashboard.setup()
