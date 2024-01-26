b4638ebc112ae09b0bd802f96034ccce9385bcb0
kevinyyca <121273910+kevinyyca@users.noreply.github.com>
2024-01-26 17:40:24 +0800

    Load telescope at start up

--- lua/chen/plugins/dashboard.lua
+++ lua/chen/plugins/dashboard.lua
@@ -1,7 +1,10 @@
 return {
-  'nvimdev/dashboard-nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  event = 'VimEnter',
+  "nvimdev/dashboard-nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "nvim-telescope/telescope.nvim"
+  },
+  event = "VimEnter",
 
   config = function()
     local dashboard = require("dashboard")
--- lua/chen/plugins/telescope.lua
+++ lua/chen/plugins/telescope.lua
@@ -5,42 +5,35 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim" },
     "nvim-tree/nvim-web-devicons",
   },
-  keys = {
-    { "<leader>ff", ":Telescope find_files<CR>" },
-    { "<leader>fr", ":Telescope oldfiles<CR>" },
-    { "<leader>fs", ":Telescope live_grep<CR>" },
-    { "<leader>fc", ":Telescope grep_string<CR>" }
-  },
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
 
     telescope.setup({
-          defaults = {
-            path_display = { "truncate " },
-            mappings = {
-              i = {
-                ["<C-k>"] = actions.move_selection_previous, -- Move to prev result
-                ["<C-j>"] = actions.move_selection_next, -- Move to next result
-                ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-              },
-            },
+      defaults = {
+        path_display = { "truncate " },
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous,     -- Move to prev result
+            ["<C-j>"] = actions.move_selection_next,         -- Move to next result
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
           },
-        })
+        },
+      },
+    })
 
     telescope.load_extension("fzf")
 
     -- Keymaps
     local keymap = vim.keymap
 
-    keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" }) -- Fuzzy find files in cwd
-    keymap.set("n", "<leader>fr", ":Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" }) -- Fuzzy find recent files
-    keymap.set("n", "<leader>fs", ":Telescope live_grep<CR>", { desc = "Find string in cwd" }) -- Find string in cwd (Requires system package: Ripgrep)
+    keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })          -- Fuzzy find files in cwd
+    keymap.set("n", "<leader>fr", ":Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })            -- Fuzzy find recent files
+    keymap.set("n", "<leader>fs", ":Telescope live_grep<CR>", { desc = "Find string in cwd" })                -- Find string in cwd (Requires system package: Ripgrep)
     keymap.set("n", "<leader>fc", ":Telescope grep_string<CR>", { desc = "Find string under cursor in cwd" }) -- Find string under cursor in cwd (Requires system package: Ripgrep)
 
     -- Change colors
-    vim.cmd( [[highlight TelescopeNormal guibg=#16161e00]] ) -- Make the background of Telescope transparent
-    vim.cmd( [[highlight TelescopeBorder guibg=#16161e00]] ) -- Make the background of Telescope borders transparent
-
+    vim.cmd([[highlight TelescopeNormal guibg=#16161e00]])   -- Make the background of Telescope transparent
+    vim.cmd([[highlight TelescopeBorder guibg=#16161e00]])   -- Make the background of Telescope borders transparent
   end,
 }
