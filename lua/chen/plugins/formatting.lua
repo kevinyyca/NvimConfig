d0e7cc6617149e6cf4eb0377ddac5eea80040e19
kevinyyca <121273910+kevinyyca@users.noreply.github.com>
2024-01-30 16:56:00 +0800

    Added formatting for Lua and Python

---
+++ lua/chen/plugins/formatting.lua
@@ -0,0 +1,22 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        python = { "black", "isort" },
+        lua = { "stylua" }
+      }
+    })
+
+    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500
+      })
+    end, { desc = "Format file or range (In visual mode)" })
+  end
+}
