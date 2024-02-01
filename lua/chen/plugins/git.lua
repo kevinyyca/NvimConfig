d47c7fa81e5416a0342c20d473fe80958dbdfe46
kevinyyca <121273910+kevinyyca@users.noreply.github.com>
2024-02-01 10:33:47 +0800

    Installed tpope/vim-fugitive

--- lua/chen/plugins/git.lua
+++ lua/chen/plugins/git.lua
@@ -1,4 +1,8 @@
 return {
+  {
+    "tpope/vim-fugitive"
+  },
+  {
   "lewis6991/gitsigns.nvim",
   config = function()
     local gitsigns = require("gitsigns")
@@ -10,4 +14,5 @@ return {
     keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview Gitsigns hunk" })                  -- Preview Gitsigns hunk
     keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle current line blame" }) -- Toggle current line blame
   end
+  }
 }
