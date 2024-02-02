4f536222e1fd75913575e382d1cbdbabd1d6a478
kevinyyca <121273910+kevinyyca@users.noreply.github.com>
2024-02-02 17:37:42 +0800

    Added a comment to lualine.lua

--- lua/chen/plugins/lualine.lua
+++ lua/chen/plugins/lualine.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  event = { "BufReadPre", "BufNewFile" },
+  event = { "BufReadPre", "BufNewFile" }, -- Only load Lualine when editing a file (dashboard hides lualine)
   config = function()
     local lualine = require("lualine")
     local lazy_status = require("lazy.status") -- To configure lazy pending updates count
