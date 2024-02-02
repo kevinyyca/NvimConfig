c3bfc6bcf510e15fd2e406de6e9efca782b19c39
kevinyyca <121273910+kevinyyca@users.noreply.github.com>
2024-02-02 16:36:50 +0800

    Only load vim-surround when editing a file

--- lua/chen/plugins/init.lua
+++ lua/chen/plugins/init.lua
@@ -1,5 +1,4 @@
 return {
   "christoomey/vim-tmux-navigator",  -- Tmux and spit window navigation
-  "tpope/vim-surround",              -- Sourround text
   "vim-scripts/ReplaceWithRegister", -- Replace text with copied text
 }
